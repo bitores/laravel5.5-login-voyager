bb30837759baac9de8f51b2f4980463a19830b69
huangzhengjie <huangzhengjie@dingtalk.com>
2017-10-07 00:22:30 +0800

    fix bug

--- app/User.php
+++ app/User.php
@@ -5,15 +5,15 @@ namespace App;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 
-use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\SoftDeletes;
-use jeremykenedy\LaravelRoles\Traits\HasRoleAndPermission;
+// use Illuminate\Database\Eloquent\Model;
+// use Illuminate\Database\Eloquent\SoftDeletes;
+// use jeremykenedy\LaravelRoles\Traits\HasRoleAndPermission;
 
 class User extends \TCG\Voyager\Models\User
 {
     use Notifiable;
-    use HasRoleAndPermission;
-    use SoftDeletes;
+    // use HasRoleAndPermission;
+    // use SoftDeletes;
 
     /**
      * The attributes that are mass assignable.
